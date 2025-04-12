5059f729e04fadbc7a52166b2ba18ca536f86260
Shashank-m-m-18 <shashankmm23csds@rnsit.ac.in>
2025-04-12 16:30:02 +0530

    Update stats.h

--- Assignments/Ass. 1/stats.h
+++ Assignments/Ass. 1/stats.h
@@ -13,8 +13,8 @@
  * @brief Definitions and macros required for the c1m1 assignment
  *
  *
- * @author Mohammed Abdelalim
- * @date 7/9/2020
+ * @author Shashank M M
+ * @date 12/04/25
  *
  */
 #ifndef __STATS_H__
